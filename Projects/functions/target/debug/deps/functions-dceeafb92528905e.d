86863a12ba10f19827bc0018c25b113ca4040485
IamGatheru <davidgatheru490@gmail.com>
2024-10-28 17:08:25 +0300

    Functions

---
+++ Projects/functions/target/debug/deps/functions-dceeafb92528905e.d
@@ -0,0 +1,5 @@
+/home/bitxyz/Public/sturdy-octo-enigma/Projects/functions/target/debug/deps/functions-dceeafb92528905e: src/main.rs
+
+/home/bitxyz/Public/sturdy-octo-enigma/Projects/functions/target/debug/deps/functions-dceeafb92528905e.d: src/main.rs
+
+src/main.rs:
---
+++ Projects/functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/bitxyz/Public/sturdy-octo-enigma/Projects/functions/target/debug/functions: /home/bitxyz/Public/sturdy-octo-enigma/Projects/functions/src/main.rs
