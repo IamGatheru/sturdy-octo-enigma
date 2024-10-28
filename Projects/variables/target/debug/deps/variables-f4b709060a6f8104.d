99db2c65e8958e116243714dad0ccc7eea3bbc6d
IamGatheru <davidgatheru490@gmail.com>
2024-10-28 11:50:00 +0300

    Variables

---
+++ Projects/variables/target/debug/deps/variables-f4b709060a6f8104.d
@@ -0,0 +1,5 @@
+/home/bitxyz/Public/sturdy-octo-enigma/Projects/variables/target/debug/deps/variables-f4b709060a6f8104: src/main.rs
+
+/home/bitxyz/Public/sturdy-octo-enigma/Projects/variables/target/debug/deps/variables-f4b709060a6f8104.d: src/main.rs
+
+src/main.rs:
---
+++ Projects/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/bitxyz/Public/sturdy-octo-enigma/Projects/variables/target/debug/variables: /home/bitxyz/Public/sturdy-octo-enigma/Projects/variables/src/main.rs
